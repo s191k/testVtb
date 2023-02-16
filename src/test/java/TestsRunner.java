834e93fbeb5237698dd268074d737a75a43b8a86
Kupyrev Alexander <alexander.kupyrev@pladform.ru>
2023-02-16 11:55:34 +0300

    +

---
+++ src/test/java/TestsRunner.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src\\test\\resources"
+        ,glue={"steps"}
+)
+
+public class TestsRunner {
+
+}
--- src/test/java/steps/MyStepdefs.java
+++ src/test/java/steps/MyStepdefs.java
@@ -1,3 +1,5 @@
+package steps;
+
 import cucumber.api.java.ru.Допустим;
 
 import static com.codeborne.selenide.Selectors.byXpath;
@@ -5,32 +7,32 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class MyStepdefs {
-    @cucumber.api.java.ru.Допустим("^Заходим на сайт втб$")
+    @Допустим("^Заходим на сайт втб$")
     public void заходимНаСайтВтб() {
         open("https://www.vtb.ru/");
     }
 
-    @cucumber.api.java.ru.Допустим("^Сначала нажимаем кнопку ВТБ-Онлайн$")
+    @Допустим("^Сначала нажимаем кнопку ВТБ-Онлайн$")
     public void сначалаНажимаемКнопкуВТБОнлайн() {
         $(byXpath("//a[text()='ВТБ-Онлайн']")).click();
     }
 
-    @cucumber.api.java.ru.Допустим("^Сначала вводим (\\d+)$")
+    @Допустим("^Сначала вводим (\\d+)$")
     public void сначалаВводим(String cardNUmberOrLogin) {
         $(byXpath("//label[text()='Номер карты или логин (УНК)']")).parent().$(byXpath("input")).sendKeys(cardNUmberOrLogin);
     }
 
-    @cucumber.api.java.ru.Допустим("^Нажимаем кнопку Далее$")
+    @Допустим("^Нажимаем кнопку Далее$")
     public void нажимаемКнопкуДалее() {
         $(byXpath("//button[text()='Далее']")).click();
     }
 
-    @cucumber.api.java.ru.Допустим("^Потом вводим (\\d+)$")
+    @Допустим("^Потом вводим (\\d+)$")
     public void потомВводим(String password) {
         $(byXpath("//label[text()='Пароль']")).parent().$(byXpath("input")).sendKeys(password);
     }
 
-    @cucumber.api.java.ru.Допустим("^Нажимаем кнопку Войти$")
+    @Допустим("^Нажимаем кнопку Войти$")
     public void нажимаемКнопкуВойти() {
         $(byXpath("//button[text()='Войти']")).click();
     }
